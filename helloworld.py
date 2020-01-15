ab74583429333f5aa7c6ceb9c88e949bdb82cd13
jameslec <jameslec@umich.edu>
2020-01-15 18:17:08 -0500

    Initial Commit

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+
+if __name__=='__main__':
+	print("Jimmy")
+	a = random.randint(0,100)
+	b = random.randint(0,100)
+	print(a)
+	print(b)
+	print("Sum = ", (a+b))
+	print("Average = ", (a+b)/2.0)
